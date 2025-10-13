f0ab0266b5395208b839f05286be9bdc974a2579
gabrmanh <gmanhani@yahoo.com>
2025-10-13 19:29:03 -0300

    Initial Commit

---
+++ src/main/kotlin/WebApplication.kt
@@ -0,0 +1,11 @@
+package org.showoff
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+open class WebApplication {
+    fun main(args: Array<String>) {
+        runApplication<WebApplication>(*args)
+    }
+}   
