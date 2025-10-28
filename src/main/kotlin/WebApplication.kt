c43360c9108fc3548b144731f006501e60e9fd6c
gabrmanh <gmanhani@yahoo.com>
2025-10-27 21:46:02 -0300

    feat: add entity scanning to spring app

--- src/main/kotlin/WebApplication.kt
+++ src/main/kotlin/WebApplication.kt
@@ -1,11 +1,15 @@
 package org.showoff
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
-@SpringBootApplication
-open class WebApplication {
-    fun main(args: Array<String>) {
-        runApplication<WebApplication>(*args)
-    }
-}   
+@SpringBootApplication(scanBasePackages = ["org.showoff"])
+@EnableJpaRepositories("org.showoff.persistence.repository")
+@EntityScan("org.showoff.entity")
+open class WebApplication 
+
+fun main(args: Array<String>) {
+    runApplication<WebApplication>(*args)
+}
